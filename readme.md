be60e244640898cc5c0f4381874688a55140f51a
robin_shaun <robin_shaun@foxmail.com>
2021-12-25 20:07:20 +0800

    add readme

---
+++ readme.md
@@ -0,0 +1,14 @@
+# DVS Event Server
+
+## Introduce
+Transport "dvs/events" ROS topic to Python, because Python ROS callback has large delay.
+
+## Dependency
+[rpg_dvs_ros](https://github.com/uzh-rpg/rpg_dvs_ros)
+
+[cppzmq](https://github.com/zeromq/cppzmq)
+
+[protobuf](https://developers.google.com/protocol-buffers/docs/cpptutorial)
+
+## Usage
+See [E2VID_ROS](https://github.com/robin-shaun/E2VID_ROS) 
--- src/event_server.cpp
+++ src/event_server.cpp
@@ -12,40 +12,50 @@
 #include <deque> 
 #include <thread>
 #include <zmq.hpp>
+
 using namespace std;
 using namespace event_array::protobuf;
-int event_window_size = 30000;
 deque<double> ts;
 deque<int32_t> x;
 deque<int32_t> y;
 deque<int32_t> polarity;
+bool start = false;
 bool ready = false;
+pthread_mutex_t event_vec_lock = PTHREAD_MUTEX_INITIALIZER;
 
-void eventCallback(const dvs_msgs::EventArray::ConstPtr &msg)
+void eventCallback(const dvs_msgs::EventArray::ConstPtr &msg, int event_window_size)
 {
+    if (!start)
+        return;
+    pthread_mutex_lock(&event_vec_lock);
     for (auto event : msg->events) 
         {
-            ready = false;
             ts.push_back(event.ts.toSec());
             x.push_back(event.x);
             y.push_back(event.y);
             polarity.push_back(event.polarity);
             if (ts.size() > event_window_size)
             {
+                ready = true;
+            }
+            if (ts.size() > 100 * event_window_size)
+            {
                 ts.pop_front();
                 x.pop_front();
                 y.pop_front();
                 polarity.pop_front();
-                ready = true;
             }
         }
+    pthread_mutex_unlock(&event_vec_lock);
 }
 
 int main(int argc, char **argv)
 {
     ros::init(argc, argv, "event_server");
     ros::NodeHandle nh_;
-    ros::Subscriber sub = nh_.subscribe("/dvs/events", 100, eventCallback);
+    int event_window_size;
+    nh_.getParam("event_window_size", event_window_size);
+    ros::Subscriber sub = nh_.subscribe<dvs_msgs::EventArray>("/dvs/events", 100, boost::bind(&eventCallback, _1, event_window_size));
     thread ros_spin([&]() {
         ros::spin();
     });
@@ -54,16 +64,15 @@ int main(int argc, char **argv)
 
     zmq::context_t context (1);
     zmq::socket_t socket (context, ZMQ_REP);
-    socket.bind ("tcp://*:10001");
+    socket.bind ("tcp://*:10002");
 
     while(ros::ok())
     {
         zmq::message_t request;
 
         // Wait for next request from client
-        socket.recv (&request);
-        std::cout << "Received Hello" << std::endl;
-
+        socket.recv(&request);
+        start = true;
         // Send reply back to client
         if (!ready)
         {
@@ -73,14 +82,20 @@ int main(int argc, char **argv)
         }
         else
         {
+            pthread_mutex_lock(&event_vec_lock);
             for (int i = 0; i < event_window_size; i++)
             {
-                auto event = event_array.add_event();
-                event->set_timestamp(ts.at(i));
-                event->set_x(x.at(i));
-                event->set_y(y.at(i));
-                event->set_polarity(polarity.at(i));
+                event_array.add_timestamp(ts.at(0));
+                ts.pop_front();
+                event_array.add_x(x.at(0));
+                x.pop_front();
+                event_array.add_y(y.at(0));
+                y.pop_front();
+                event_array.add_polarity(polarity.at(0));
+                polarity.pop_front();
             }
+            pthread_mutex_unlock(&event_vec_lock);
+
             string data;
             event_array.SerializeToString(&data);
             zmq::message_t reply(data.size());
@@ -88,6 +103,7 @@ int main(int argc, char **argv)
             socket.send(reply);
 
             event_array.Clear();
+            ready = false;
         }
 
     }    
